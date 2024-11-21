225686d360f82711d6c5407577a2887280f2460d
defolym3 <defolym3@gmail.com>
2024-11-21 13:47:18 +0700

    Update addresses.ts

--- src/util/addresses.ts
+++ src/util/addresses.ts
@@ -5,7 +5,7 @@ export type AddressMap = { [chainId: number]: string | undefined };
 
 export const SWAP_ROUTER_02_ADDRESSES = (chainId: number): string => {
   console.log(`SWAP_ROUTER_02_ADDRESSES: not implemented. chain id ${chainId}`);
-  return '';
+  return '0xEE0A7E2100F4b98f549230E0e843041A96f018a9'; //minato
 };
 
 export const OVM_GASPRICE_ADDRESS = '0x420000000000000000000000000000000000000F';
